dae5a37e0ab7ef6db95b06ead4724b6ab15a6570
Daniel de Oliveira <97067805+nadjiel@users.noreply.github.com>
2025-04-20 12:57:12 -0300

    :sparkles: feat: constructor now uses custom filters

--- godocs/doc_constructor.py
+++ godocs/doc_constructor.py
@@ -1,18 +1,47 @@
 from jinja2 import Environment, PackageLoader, FileSystemLoader, select_autoescape
+from filters import *
 
 env = Environment(
     loader=FileSystemLoader("godocs/templates/rst/"),
     autoescape=select_autoescape()
 )
 
-template = env.get_template("heading.rst")
+env.filters["make_code_member_label_target"] = make_code_member_label_target
+env.filters["join_code_member_name"] = join_code_member_name
+env.filters["make_code_member_ref"] = make_code_member_ref
+env.filters["make_code_member_type_ref"] = make_code_member_type_ref
+env.filters["make_property_signature"] = make_property_signature
+env.filters["make_method_signature"] = make_method_signature
+
+template = env.get_template("class_reference.rst")
 
 context = {
   "ref_prefix": "godocs",
   "class": {
     "name": "Class",
-    "parents": [ "SuperClass", "GrandClass", "ImenseClass" ],
+    "parents": [ "SuperClass", "GrandClass", "ImenseClass.InnerClass" ],
     "brief_description": "This is a Class",
+    "description": "This is a long description.",
+    "properties": [
+      {
+        "name": "property_a",
+        "type": "Array[String]",
+        "default": '""',
+      }
+    ],
+    "methods": [
+      {
+        "name": "method_a",
+        "type": "Array[String]",
+        "args": [
+          {
+            "name": "arg1",
+            "type": "int",
+            "default": "0",
+          }
+        ],
+      }
+    ],
   },
 }
 
