dbf714a4f566a589964ee3c4e8de227423bed42d
Daniel de Oliveira <97067805+nadjiel@users.noreply.github.com>
2025-08-26 21:24:39 -0300

    :sparkles: feat: implemented xml_parser

---
+++ src/godocs/parser/xml_parser.py
@@ -0,0 +1,59 @@
+from pathlib import Path
+import xml.etree.ElementTree as ET
+
+
+def parse_file(path: Path):
+    """
+    Parses an XML file from a given path and returns an ElementTree object.
+
+    Args:
+      path: Path to the XML file.
+
+    Returns:
+      Parsed ElementTree containing the XML data.
+
+    Raises:
+        FileNotFoundError: If the file at the given path does not exist.
+        xml.etree.ElementTree.ParseError: If the file is not a valid XML document.
+    """
+
+    return ET.parse(path)
+
+
+def parse_folder(path: Path):
+    """
+    Parses all XML files from a given path and returns a list with ElementTree objects.
+
+    Args:
+      path: Path to the folder containing XML files.
+
+    Returns:
+      List with the parsed ElementTrees containing the XML data.
+
+    Raises:
+        NotADirectoryError: If the path doesn't point to a directory.
+    """
+
+    return [parse_file(subpath) for subpath in path.glob("*.xml")]
+
+
+def parse(path: Path):
+    """
+    Parses one or more XML files from a given path.
+
+    If the path points to a file, parses that file.
+    If the path points to a directory, parses all XML files in the directory.
+    Returns a list of parsed ElementTree objects.
+
+    Args:
+      path: Path to an XML file or a directory containing XML files.
+
+    Returns:
+      A list of ElementTree objects parsed from the XML files.
+
+    Raises:
+        FileNotFoundError: If the file at the given path does not exist.
+        xml.etree.ElementTree.ParseError: If the file is not a valid XML document.
+    """
+
+    return [parse_file(path)] if path.is_file() else parse_folder(path)
