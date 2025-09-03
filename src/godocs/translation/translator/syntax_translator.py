2278be21ba12a4892e0c05d232d7d44981fd1115
Daniel O. Sousa <97067805+nadjiel@users.noreply.github.com>
2025-09-02 21:47:51 -0300

    :books: docs: described the syntax_translator module

--- src/godocs/translation/translator/syntax_translator.py
+++ src/godocs/translation/translator/syntax_translator.py
@@ -1,17 +1,31 @@
 from abc import ABC, abstractmethod
 
-from godocs.translation.ast import (
-    TextNode,
-    TagNode,
-)
+from godocs.translation import ast
 
 
 class SyntaxTranslator(ABC):
+    """
+    Abstract base class for translating `ast` nodes.
+
+    This class defines a interface for translating two types of
+    nodes into string representations: the `TextNode` and the `TagNode`.
+
+    Subclasses must implement the `translate_text` and `translate_tag` methods
+    for defining how those nodes should be translated.
+    """
 
     @abstractmethod
-    def translate_text(self, node: TextNode) -> str:
+    def translate_text(self, node: ast.TextNode) -> str:
+        """
+        Abstract method to translate a `TextNode` into its string representation.
+        """
+
         pass
 
     @abstractmethod
-    def translate_tag(self, node: TagNode) -> str:
+    def translate_tag(self, node: ast.TagNode) -> str:
+        """
+        Abstract method to translate a `TagNode` into its string representation.
+        """
+
         pass
