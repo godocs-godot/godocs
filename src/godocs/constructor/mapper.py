24a9bce95e489ad5f64e5c452de32d13424415f3
Daniel O. Sousa <97067805+nadjiel@users.noreply.github.com>
2025-09-08 17:13:54 -0300

    :sparkles: feat: created constructor mapper

---
+++ src/godocs/constructor/mapper.py
@@ -0,0 +1,29 @@
+from os import PathLike
+from .jinja_constructor import JinjaConstructor
+from godocs.util import module
+
+
+def get_constructor(name: str | PathLike[str]):
+    """
+    Returns a `Constructor` instance based on the `name` passed.
+
+    Currently, the only supported constructors is the `jinja`.
+
+    If it's desired to get a custom constructor, its path can
+    be passed in the `name` parameter.
+
+    Custom constructors should have a class called `Constructor`
+    which should extend the base `Constructor` class.
+
+    If no `Constructor` class is exposed, a `NotImplementedError`
+    is raised.
+    """
+
+    match name:
+        case "jinja": return JinjaConstructor()
+        case _:
+            try:
+                return module.load("constructor", name).Constructor()
+            except AttributeError:
+                raise NotImplementedError(
+                    "Constructor member not found. Custom constructors should have a Constructor class")
