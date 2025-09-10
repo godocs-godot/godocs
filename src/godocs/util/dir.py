2d2c32ad42ac9c38b01af6690be7a9e41e0915d5
Daniel O. Sousa <97067805+nadjiel@users.noreply.github.com>
2025-09-09 22:39:18 -0300

    :sparkles: feat: added general get_subitems util

--- src/godocs/util/dir.py
+++ src/godocs/util/dir.py
@@ -1,11 +1,50 @@
 from os import PathLike
 from pathlib import Path
+from typing import Callable
+
+
+def get_subitems(
+    path: str | PathLike[str],
+    exclude: list[str] | None = None,
+    include: list[str] | None = None,
+    predicate: Callable[[Path], bool] | None = None,
+) -> list[Path]:
+    """
+    Returns a `list` with the `Paths` of the subitems
+    (both files and or directories) of the `path` passed,
+    excluding the ones from the `exclude` list and
+    including only the ones from the `include` list (if any).
+    """
+
+    if exclude is None:
+        exclude = []
+    if include is None:
+        include = []
+    if predicate is None:
+        predicate = (lambda _: True)
+
+    path = Path(path)
+
+    subitems: list[Path] = []
+
+    for p in path.iterdir():
+        if p.name in exclude:
+            continue
+        if not p.name in include:
+            continue
+        if not predicate(p):
+            continue
+
+        subitems.append(p)
+
+    return subitems
 
 
 def get_subdirs(
     path: str | PathLike[str],
     exclude: list[str] | None = None,
     include: list[str] | None = None,
+    predicate: Callable[[Path], bool] | None = None,
 ) -> list[Path]:
     """
     Returns a `list` with the `Paths` of the subdirectories of
@@ -14,23 +53,9 @@ def get_subdirs(
     (if any).
     """
 
-    if exclude is None:
-        exclude = []
-    if include is None:
-        include = []
-
-    path = Path(path)
-
-    subdirs: list[Path] = []
-
-    for p in path.iterdir():
-        if not p.is_dir():
-            continue
-        if p.name in exclude:
-            continue
-        if not p.name in include:
-            continue
-
-        subdirs.append(p)
-
-    return subdirs
+    return get_subitems(
+        path,
+        exclude,
+        include,
+        lambda p: p.is_dir() and predicate(p) if predicate is not None else True
+    )
