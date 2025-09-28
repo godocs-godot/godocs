c0461016423280e12e3b4dd2d75a9535422ff292
Daniel O. Sousa <97067805+nadjiel@users.noreply.github.com>
2025-09-28 17:30:58 -0300

    :books: docs (cli_command): added descriptions

--- src/godocs/cli/command/cli_command.py
+++ src/godocs/cli/command/cli_command.py
@@ -3,7 +3,23 @@ from typing import Any
 
 
 class CLICommand(ABC):
+    """
+    Base class that represents a CLI command or subcommand.
+
+    The `register` method should be implemented to connect
+    the logic of this command as an `argparse` parser or
+    subparser.
+    """
 
     @abstractmethod
-    def register(self, subparsers: Any):
+    def register(self, subparsers: Any | None):
+        """
+        Abstract method that takes a `subparsers` instance
+        (from `argparse.ArgumentParser.add_subparsers`) and
+        should append a subparser to them.
+
+        For a root command implementation, the `subparsers`
+        parameter may be null, since it isn't a subparser
+        as opposed to a main parser.
+        """
         pass
