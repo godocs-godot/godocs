59ae19e858de734c739e3c4ea672310ed3ea7249
Daniel O. Sousa <97067805+nadjiel@users.noreply.github.com>
2025-09-28 19:35:00 -0300

    :bug: fix (cli): problem with unknown plugin options

--- src/godocs/cli/command/app_command.py
+++ src/godocs/cli/command/app_command.py
@@ -67,7 +67,9 @@ class AppCommand(CLICommand):
     def register(self, subparsers: Any | None = None):
         """
         Creates the `parser` for this `AppCommand` and
-        registers its options, parameters and subcommands.
+        registers the `--plugin` or `-p` option, as well
+        as sets the help printing function as the default
+        behavior when nothing else is chosen.
         """
 
         self.parser = ArgumentParser(description="Godot Docs generator CLI")
@@ -78,23 +80,44 @@ class AppCommand(CLICommand):
         )
         self.parser.set_defaults(func=self.exec)
 
+    def register_subparsers(self):
+        """
+        Registers the subparsers for this `AppCommand`.
+
+        This method was separated from the `register` because
+        this way parsing can be realized before and after
+        the native subcommands are registered, avoiding errors
+        with unknown subparsers that are registered after in
+        plugin code.
+        """
+
         self.subparsers = self.parser.add_subparsers(
             title="command", description="The command to execute.")
 
         self.commands["construct"].register(self.subparsers)
 
     def main(self, argv: Optional[Sequence[str]] = None):
+        """
+        Entrypoint for the `AppCommand` to be parsed and executed,
+        as well as to register plugins in the way.
+        """
+
+        # Creates the main parser.
         self.register()
 
-        args, rest = self.parser.parse_known_args(argv)
+        # Parses args looking solely for the --plugin option.
+        args, _ = self.parser.parse_known_args(argv)
 
+        # Registers main parser's subparsers.
+        self.register_subparsers()
+
+        # If a plugin script was provided, execute it.
         if args.plugin:
             self.register_plugin(args.plugin)
 
-        args, rest = self.parser.parse_known_args(argv)
+        # Parse again, this time with plugin features.
+        args, _ = self.parser.parse_known_args(argv)
 
-        print(args)
-        print(rest)
-
+        # Execute main args.func.
         if args.func is not None:
             args.func(args)
