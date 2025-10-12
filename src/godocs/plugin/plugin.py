236b1bb54744155d7d3c5ff131425f1675d06b7c
Daniel O. Sousa <97067805+nadjiel@users.noreply.github.com>
2025-10-11 21:49:37 -0300

    :sparkles: feat: added plugin class

---
+++ src/godocs/plugin/plugin.py
@@ -0,0 +1,20 @@
+from abc import ABC, abstractmethod
+from godocs.cli import CLICommand
+
+
+class Plugin(ABC):
+    """
+    This class allows the extension of the Godocs package
+    by other packages that use the
+    `[project.entry-points."godocs.plugins"]` field in their
+    `pyproject.toml` to expose their `Plugin` implementation.
+    """
+
+    @abstractmethod
+    def register(self, app: CLICommand):
+        """
+        This abstract method should be implemented to register
+        new plugins to the Godocs package.
+        """
+
+        pass
